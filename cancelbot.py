4f12930a9bb57c293c25eecc81c77c49394363c3
heefpuy <39878411+heefpuy@users.noreply.github.com>
2018-07-07 19:20:57 +0700

    Add files via upload

--- cancelbot.py
+++ cancelbot.py
@@ -2,6 +2,10 @@ from linepy import *
 import time
 
 cl = LINE('apaankeek18@gmail.com','Muhamad18')
+#cl = LINE('EunGJlvq37U5sDc2Xjp1.r7LQgKdASAz0gOQcFUAR4q.zO3QLPsg5MJojipCN1goRhUWX/AxEOQ+3X2DfcenY04=')
+
+#cl.log("Auth Token : " + str(line.authToken))
+#cl.log("Timeline Token : " + str(line.tl.channelAccessToken))
 poll = OEPoll(cl)
 
 while True:
@@ -11,12 +15,12 @@ while True:
             for op in ops:
                 if (op.type == 13):
                     cl.acceptGroupInvitation(op.param1)
-                    cl.sendMessage(op.param1,'Cancelling')
+                    cl.sendMessage(op.param1,'Cancelling..')
                 if (op.type == 25):
                     msg = op.message
-                    if (msg.text.lower() == 'start'):
+                    if (msg.text.lower() == 'start!'):
                         s = time.time()
-                        cl.sendMessage('Speed')
+                        cl.sendMessage('Speed!')
                         e = time.time() - s
                         cl.sendMessage('{:.14f}'.format(e))
                     if ('cancelling~' in msg.text.lower()):
@@ -27,7 +31,7 @@ while True:
                                 cl.cancelGroupInvitation(msg.to,[mid])
                             except Exception as e:
                                 pass
-                        cl.sendMessage(msg.to,'Group has been Cleaned\nline://ti/p/~heefpuy')
+                        cl.sendMessage(msg.to,'Done!\nline://ti/p/~heefpuy')
                         cl.leaveGroup(msg.to)
                 poll.setRevision(op.revision)
     except Exception as e:
