39233a0e75843f6f2c6eb0f9417c00d5d20ef4fd
rinda18 <41496540+rinda18@users.noreply.github.com>
2018-07-31 17:55:43 +0700

    Update cancelbot.py

--- cancelbot.py
+++ cancelbot.py
@@ -1,7 +1,7 @@
 from linepy import *
 import time
 
-cl = LINE('mkhadaffy1805@gmail.com','Muhamad18')
+cl = LINE('@gmail.com','')
 #cl = LINE('EunGJlvq37U5sDc2Xjp1.r7LQgKdASAz0gOQcFUAR4q.zO3QLPsg5MJojipCN1goRhUWX/AxEOQ+3X2DfcenY04=')
 
 #cl.log("Auth Token : " + str(line.authToken))
